de79f6bef5875c716e26e74ed73c3e0d031f2b8e
dF <userdf@live.ru>
2022-10-09 05:07:25 +0300

    commit

---
+++ wwwroot/I/HATE.js
@@ -0,0 +1,21 @@
+﻿// https://www.syncfusion.com/faq/blazor/general/how-do-i-generate-and-save-a-file-client-side-using-blazor
+// save file
+
+//function saveFile(file, Content) {
+//    var link = document.createElement('a');
+//    link.download = name;
+//    link.href = "data:text/plain;charset=utf-8," + encodeURIComponent(Content)
+//    document.body.appendChild(link);
+//    link.click();
+//    document.body.removeChild(link);
+//}
+
+
+function saveAsFile(filename, bytesBase64) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + bytesBase64;
+    document.body.appendChild(link); // Needed for Firefox
+    link.click();
+    document.body.removeChild(link);
+}
